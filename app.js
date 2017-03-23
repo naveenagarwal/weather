4cf54c88a8b7af0426db4cc67d2e0e70990fc14f
Naveen Agarwal <naveenagarwal287@gmail.com>
2017-03-23 16:08:56 +0530

    extra packages removed package removed

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
 var express = require('express');
 var path = require('path');
 var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
 
 var middleWears = require('./middlewear/middlewear');
 
@@ -16,9 +14,6 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
 app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 // middlewear to tell the format of the request
